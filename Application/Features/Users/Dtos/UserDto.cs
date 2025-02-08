3bc4a50e9312a08d9081246acf0924c41504275e
danieladane2196@gmail.com <danieladane2196@gmail.com>
2025-02-08 19:48:02 +0300

    feat(users): update user dto

---
+++ Application/Features/Users/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Features.Users.Dtos
+{
+    public sealed record UserDto(Guid Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string PhoneNumber,
+        string Status,
+        DateTime CreatedAt,
+        DateTime UpdatedAt
+        );
+}

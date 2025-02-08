82f3b30a8c0fe00e2aa6bb8f586ca6cb641c8b5f
danieladane2196@gmail.com <danieladane2196@gmail.com>
2025-02-08 19:49:16 +0300

    feat(domain): update UserErrors UserAlreadyExists error type

---
+++ Domain/Errors/OrganizerErrors.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+
+namespace Domain.Errors
+{
+    public static class OrganizerErrors
+    {
+        public static Error OrganzierNotFound => Error.NotFound("Organzier.NotFound", "Organzier not found");
+        public static Error OrganizerAlreadyExists => Error.Validation("Organzier.AlreadyExists", "Organzier already exists");
+    }
+}
--- Domain/Errors/UserErrors.cs
+++ Domain/Errors/UserErrors.cs
@@ -5,7 +5,7 @@ namespace Domain.Errors
     public static class UserErrors
     {
         public static Error UserNotFound => Error.NotFound("User.NotFound", "User not found");
-        public static Error UserAlreadyExists => Error.NotFound("User.AlreadyExists", "User already exists");
+        public static Error UserAlreadyExists => Error.Validation("User.AlreadyExists", "User already exists");
         public static Error UserUnAuthenticated => Error.Unauthorized("User.Unauthorized", "User is not authorized");
         public static Error InvalidUserId => Error.Validation("User.InvalidUserId", "User id is not valid");
     }

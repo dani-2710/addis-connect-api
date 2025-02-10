4087c9c0fe55c8026b9e77716992927da8862d40
danieladane2196@gmail.com <danieladane2196@gmail.com>
2025-02-10 16:25:19 +0300

    feat(presentation): add DiscountsController

---
+++ Presentation/Controllers/DiscountsController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Discounts.CreateDiscount;
+using Application.Features.Discounts.UpdateDiscount;
+using Domain.Constants;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Authorize(Roles = $"{UserRoles.Organizer},{UserRoles.Admin}")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController(IMediator mediator) : BaseController
+    {
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateDiscount(CreateDiscountCommand command)
+        {
+            var response = await mediator.Send(command);
+
+            return response.Match(result => Created("", result), Problem);
+        }
+
+        [HttpPost("{id:guid}/update")]
+        public async Task<IActionResult> CreateDiscount([FromRoute] Guid id,[FromBody] UpdateDiscountCommand command)
+        {
+            command = command with { Id = id };
+            var response = await mediator.Send(command);
+
+            return response.Match(Ok, Problem);
+        }
+    }
+}

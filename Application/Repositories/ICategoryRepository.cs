6a603ad2cc64d9f60172d01b8fee5ff91f7c0fac
danieladane2196@gmail.com <danieladane2196@gmail.com>
2025-02-10 10:27:16 +0300

    feat(application): add ICategoriesRepository interface

---
+++ Application/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Application.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateCategoryAsync(Category category);
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category?> GetCategoryByIdAsync(Guid id);
+        Task<Category?> UpdateCategoryAsync(Guid id, string? name, string? icon, string? status);
+
+    }
+}

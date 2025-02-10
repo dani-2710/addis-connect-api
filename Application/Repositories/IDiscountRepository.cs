43fdd27b0b26ef7130dd6ed01bd95118214b1690
danieladane2196@gmail.com <danieladane2196@gmail.com>
2025-02-10 16:21:26 +0300

    feat(application): add IDiscountRepository interface

---
+++ Application/Repositories/IDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+namespace Application.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Discount> CreateDiscountAsync(Discount discount);
+        Task<Discount?> UpdateDiscountAsync(
+            Guid id,
+            string? amountType,
+            decimal? amount,
+            DateTime? ValidFrom,
+            DateTime? ValidTo);
+    }
+}
